c3f2e24f17e3719421f3c3c7904e0497ae030237
itsdangerous <danger121@naver.com>
2022-12-04 19:51:51 +0900

    make Login API

--- login/views.py
+++ login/views.py
@@ -2,45 +2,40 @@
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializer import LoginUserSerializer
 from .models import LoginUser
 from django.contrib.auth.hashers import make_password, check_password
 
-class AppLogin(APIView) :
-    def post(self, request) :
+
+class AppLogin(APIView):
+    def post(self, request):
         user_id = request.data.get('user_id', "")
         user_pw = request.data.get('user_id', "")
         user = LoginUser.objects.filter(user_id=user_id).first()
 
-        if user is None :
+        if user is None:
             return Response(dict(msg="해당 ID의 사용자가 없습니다."))
-        if check_password(user_pw, user.user_pw) :
-            return Response(dict(msg="로그인 성공"))
-        else :
+        if check_password(user_pw, user.user_pw):
+            return Response(dict(msg="로그인 성공", user_id=user.user_id, birth_day=user.birth_day,
+                                 gender=user.gender, email=user.email, name=user.name, age=user.age))
+        else:
             return Response(dict(msg="로그인 실패. 패스워드 불일치!!"))
 
-class RegistUser(APIView) :
-    def post(self, request):
-        user_id = request.data.get('user_id', "")# 클라이언트에서 올리는 user_id
-        user_pw = request.data.get('user_pw', "")# 클라이언트에서 올리는 user_pw
-        user_pw_crypted = make_password(user_pw) # 암호화
 
-        if LoginUser.objects.filter(user_id=user_id).exists() : # LoginUser 모델의 object의 id 중복 처리
-            # db에 있는 값 출력할 때, 어떻게 나오는지 보기 위해 user 객체에 담음
-            user = LoginUser.objects.filter(user_id=user_id).first()
+class RegistUser(APIView):
+    def post(self, request):
+        serializer = LoginUserSerializer(request.data)
+
+        if LoginUser.objects.filter(user_id=serializer.data['user_id']).exists():
+            # DB에 있는 값 출력할 때 어떻게 나오는지 보려고 user 객체에 담음
+            user = LoginUser.objects.filter(user_id=serializer.data['user_id']).first()
             data = dict(
-                msg = "이미 존재하는 아이디입니다.",
-                user_id = user.user_id,
-                user_pw = user.user_pw
+                msg="이미 존재하는 아이디입니다.",
+                user_id=user.user_id,
+                user_pw=user.user_pw
             )
             return Response(data)
 
-        LoginUser.objects.create(user_id=user_id, user_pw=user_pw_crypted) # LoginUser 모델에 새로운 object 생성
+        user = serializer.create(request.data)
 
-        # 클라이언트한테 내려줄 데이터 정의
-
-        data = dict(
-            user_id=user_id,
-            user_pw=user_pw_crypted
-         )
-
-        return Response(data=data)
+        return Response(data=LoginUserSerializer(user).data)
